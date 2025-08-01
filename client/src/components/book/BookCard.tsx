9e09652e1584bcb69a07724decca36055ca8a14d
lecto17 <khwoo_rpsit@naver.com>
2025-08-01 16:02:38 +0900

    chore: 도서 카드 클릭 시 url 이동

--- client/src/components/book/BookCard.tsx
+++ client/src/components/book/BookCard.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import { Book } from "@/types/book"
 import { bookCardStyle, bookImageContainerStyle } from "@/styles/bookList.styles"
+import Link from "next/link"
+import { concatIsbn } from "@/utils/utils"
 
 
 type BookCardProps = {
@@ -9,15 +11,17 @@ type BookCardProps = {
 
 const BookCard = ({ book }: BookCardProps) => {
   return (
-    <div css={bookCardStyle}>
-      <div css={bookImageContainerStyle}>
-        <Image src={book.thumbnail} alt={book.title} width={100} height={100} />
+    <Link href={`/books/${concatIsbn(book.isbn)}?step=1`} passHref>
+      <div css={bookCardStyle}>
+        <div css={bookImageContainerStyle}>
+          <Image src={book.thumbnail} alt={book.title} width={100} height={100} />
+        </div>
+        <h3>{book.title}</h3>
+        <p>{book.authors.join(", ")}</p>
+        <p>{book.publisher}</p>
+        <p>{book.price}</p>
       </div>
-      <h3>{book.title}</h3>
-      <p>{book.authors.join(", ")}</p>
-      <p>{book.publisher}</p>
-      <p>{book.price}</p>
-    </div>
+    </Link>
   )
 }
 
