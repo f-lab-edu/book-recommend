631e245665858dae41493970cfee31c30c05061b
lecto17 <khwoo_rpsit@naver.com>
2025-08-01 16:23:05 +0900

    feat: 도서 평가 컴포넌트 생성

---
+++ client/src/styles/bookStepper.styles.ts
@@ -0,0 +1,24 @@
+import { css } from "@emotion/react";
+
+export const bookStepperContainerStyle = css`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f0f0f0;
+  justify-content: center;
+`;
+
+export const bookStepperContentStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 600px;
+`;
+
+export const bookStepperButtonWrapperStyle = css`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
