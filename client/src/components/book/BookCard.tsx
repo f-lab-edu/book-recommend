f7d3c9ebad93659ba8cdfa68b2d409522e02ed54
lecto17 <khwoo_rpsit@naver.com>
2025-08-02 11:15:29 +0900

    chore: BookCard 메모이제이션

--- client/src/components/book/BookCard.tsx
+++ client/src/components/book/BookCard.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 import { Book } from "@/types/book"
 import { bookCardStyle, bookImageContainerStyle } from "@/styles/bookList.styles"
+import React from "react"
 
 
 type BookCardProps = {
   book: Book
 }
 
-const BookCard = ({ book }: BookCardProps) => {
+const BookCard = React.memo(({ book }: BookCardProps) => {
   return (
     <div css={bookCardStyle}>
       <div css={bookImageContainerStyle}>
@@ -19,6 +20,13 @@ const BookCard = ({ book }: BookCardProps) => {
       <p>{book.price}</p>
     </div>
   )
-}
+}, (prevProps, nextProps) => {
+  return (
+    prevProps.book.title === nextProps.book.title &&
+    prevProps.book.sale_price === nextProps.book.sale_price &&
+    prevProps.book.thumbnail === nextProps.book.thumbnail &&
+    prevProps.book.status === nextProps.book.status
+  );
+})
 
 export default BookCard
