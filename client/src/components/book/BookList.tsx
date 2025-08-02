00cd2f71c482e0f07da3455a9355204126184649
lecto17 <khwoo_rpsit@naver.com>
2025-08-02 11:20:36 +0900

    chore: 도서 목록 결과 빈 배열일 경우 결과 없음 출력

--- client/src/components/book/BookList.tsx
+++ client/src/components/book/BookList.tsx
@@ -6,10 +6,14 @@ import InfiniteScroll from '../common/InfiniteScroll';
 const BookList = () => {
   const { data: books, isFetchingNextPage, fetchNextPage, hasNextPage } = useBookList();
 
+  if (books.length === 0) {
+    return <div>도서가 없습니다.</div>;
+  }
+
   return (
     <>
       <div css={bookListStyle}>
-        {books && books.map((book) => (
+        {books.map((book) => (
           <BookCard key={book.isbn}
             book={book}
           />
