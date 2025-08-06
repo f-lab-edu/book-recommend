d3a1bb25ebd5b7ac98cd86d2fc0d7b6989ca9eb3
lecto17 <khwoo_rpsit@naver.com>
2025-08-06 09:47:13 +0900

    refactor: queryClient 선언 위치와 방식 수정

--- client/src/provider/Provider.tsx
+++ client/src/provider/Provider.tsx
@@ -3,11 +3,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "@/theme";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createQueryClient } from "@/constants/queryClient";
-
-// 전역 QueryClient 인스턴스 생성
-const queryClient = createQueryClient();
+import { useState } from "react";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => createQueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
