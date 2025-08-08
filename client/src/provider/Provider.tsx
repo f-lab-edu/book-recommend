610c405915f2441941e9247ac21f87898c5f2683
lecto17 <khwoo_rpsit@naver.com>
2025-08-08 11:54:22 +0900

    chore: 브라우저 queryClient 생성 방식 수정

--- client/src/provider/Provider.tsx
+++ client/src/provider/Provider.tsx
@@ -1,19 +1,26 @@
 import { ThemeProvider } from "@emotion/react";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "@/theme";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createQueryClient } from "@/constants/queryClient";
 import { useState } from "react";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => createQueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60,
+            retry: 1,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        {children}
-      </ThemeProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
