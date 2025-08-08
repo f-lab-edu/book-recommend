ad80c47c2d53d6ba8d05e79be01e3c4fc3a42f5f
lecto17 <khwoo_rpsit@naver.com>
2025-08-08 12:51:32 +0900

    chore: 단계별 평가 컴포넌트 내 isbn, step 가져오기

--- client/src/components/book/BookEvaluationStepper.tsx
+++ client/src/components/book/BookEvaluationStepper.tsx
@@ -2,25 +2,36 @@ import BookDetail from "./BookDetail";
 import BookDetailSkeleton from "./BookDetailSkeleton";
 import useStepNavigation from "@/hooks/useStepNavigation";
 import SuspenseBoundary from "../common/SuspenseBoundary";
-import { bookStepperButtonWrapperStyle, bookStepperContainerStyle, bookStepperContentStyle } from "@/styles/bookStepper.styles";
+import {
+  bookStepperButtonWrapperStyle,
+  bookStepperContainerStyle,
+  bookStepperContentStyle,
+} from "@/styles/bookStepper.styles";
 import SwitchCases from "../common/SwitchCases";
-import { useStepValidation } from "@/hooks/useStepValidation";
 import { BookDetailErrorFallback } from "../common/ErrorFallbacks";
+import { useRouter } from "next/router";
 
 export const STEP_COMPONENTS = {
-  '1': <div>평가 폼0 (구현 예정)</div>,
-  '2': <div>평가 폼1 (구현 예정)</div>,
-  '3': <div>평가 폼2 (구현 예정)</div>,
-  '4': <div>평가 폼3 (구현 예정)</div>,
-  '5': <div>평가 폼4 (구현 예정)</div>,
-}
+  "1": <div>평가 폼0 (구현 예정)</div>,
+  "2": <div>평가 폼1 (구현 예정)</div>,
+  "3": <div>평가 폼2 (구현 예정)</div>,
+  "4": <div>평가 폼3 (구현 예정)</div>,
+  "5": <div>평가 폼4 (구현 예정)</div>,
+};
 
 export default function BookEvaluationStepper() {
+  const router = useRouter();
+  const { isbn, step } = router.query;
+
   const minStep = 1;
   const maxStep = Object.keys(STEP_COMPONENTS).length;
 
-  const { step, isbn } = useStepValidation();
-  const { handlePrevious, handleNext } = useStepNavigation({ isbn, step, minStep, maxStep });
+  const { handlePrevious, handleNext } = useStepNavigation({
+    isbn: isbn as string,
+    step: (step as string) || "1",
+    minStep,
+    maxStep,
+  });
 
   return (
     <div css={bookStepperContainerStyle}>
@@ -29,11 +40,11 @@ export default function BookEvaluationStepper() {
           loading={<BookDetailSkeleton />}
           rejectedFallback={BookDetailErrorFallback}
         >
-          <BookDetail isbn={isbn} />
+          <BookDetail isbn={isbn as string} />
         </SuspenseBoundary>
 
         <SwitchCases
-          value={step}
+          value={(step as string) || "1"}
           cases={STEP_COMPONENTS}
           fallback={<div>잘못된 단계입니다.</div>}
         />
