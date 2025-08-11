44d83248697820d3493adf15e2df8d059e3a87dd
lecto17 <khwoo_rpsit@naver.com>
2025-08-11 12:18:26 +0900

    chore: 단계별 검증 컴포넌트 상위에 FormProvider 래핑

--- client/src/components/book/BookEvaluationStepper.tsx
+++ client/src/components/book/BookEvaluationStepper.tsx
@@ -1,27 +1,18 @@
-import BookDetail from "./BookDetail";
-import BookDetailSkeleton from "./BookDetailSkeleton";
-import useStepNavigation from "@/hooks/useStepNavigation";
-import SuspenseBoundary from "../common/SuspenseBoundary";
-import { bookStepperButtonWrapperStyle, bookStepperContainerStyle, bookStepperContentStyle } from "@/styles/bookStepper.styles";
-import SwitchCases from "../common/SwitchCases";
-import { useStepValidation } from "@/hooks/useStepValidation";
-import { BookDetailErrorFallback } from "../common/ErrorFallbacks";
-import BookStatusPeriodValidation from "../evaluation/BookStatusPeriodValidation";
-
-export const STEP_COMPONENTS = {
-  '1': <BookStatusPeriodValidation />,
-  '2': <div>평가 폼1 (구현 예정)</div>,
-  '3': <div>평가 폼2 (구현 예정)</div>,
-  '4': <div>평가 폼3 (구현 예정)</div>,
-  '5': <div>평가 폼4 (구현 예정)</div>,
-}
+import BookDetail from './BookDetail';
+import BookDetailSkeleton from './BookDetailSkeleton';
+import SuspenseBoundary from '../common/SuspenseBoundary';
+import { bookStepperContainerStyle, bookStepperContentStyle } from '@/styles/bookStepper.styles';
+import SwitchCases from '../common/SwitchCases';
+import { useStepValidation } from '@/hooks/useStepValidation';
+import { BookDetailErrorFallback } from '../common/ErrorFallbacks';
+import BookStatusPeriodValidation from '../evaluation/BookStatusPeriodValidation';
+import { FormProvider, useForm } from 'react-hook-form';
+import StepperNavigation from './StepperNavigation';
 
 export default function BookEvaluationStepper() {
-  const minStep = 1;
-  const maxStep = Object.keys(STEP_COMPONENTS).length;
-
   const { step, isbn } = useStepValidation();
-  const { handlePrevious, handleNext } = useStepNavigation({ isbn, step, minStep, maxStep });
+
+  const form = useForm();
 
   return (
     <div css={bookStepperContainerStyle}>
@@ -33,16 +24,20 @@ export default function BookEvaluationStepper() {
           <BookDetail isbn={isbn} />
         </SuspenseBoundary>
 
-        <SwitchCases
-          value={step}
-          cases={STEP_COMPONENTS}
-          fallback={<div>잘못된 단계입니다.</div>}
-        />
-
-        <div css={bookStepperButtonWrapperStyle}>
-          <button onClick={handlePrevious}>이전</button>
-          <button onClick={handleNext}>다음</button>
-        </div>
+        <FormProvider {...form}>
+          <SwitchCases
+            value={step}
+            cases={{
+              '1': <BookStatusPeriodValidation />,
+              '2': <div>평가 폼1 (구현 예정)</div>,
+              '3': <div>평가 폼2 (구현 예정)</div>,
+              '4': <div>평가 폼3 (구현 예정)</div>,
+              '5': <div>평가 폼4 (구현 예정)</div>,
+            }}
+            fallback={<div>잘못된 단계입니다.</div>}
+          />
+          <StepperNavigation />
+        </FormProvider>
       </div>
     </div>
   );
