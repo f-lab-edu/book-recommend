dd682dcc372312f27f1fd7a260e0d2f7e0ecd431
lecto17 <khwoo_rpsit@naver.com>
2025-08-19 11:18:17 +0900

    chore: rating 변경 시 review에 대한 validation 다시 되도록 작업

--- client/src/components/evaluation/BookReviewField.tsx
+++ client/src/components/evaluation/BookReviewField.tsx
@@ -1,11 +1,17 @@
 import { css } from '@emotion/react';
 import { useFormContext } from 'react-hook-form';
 import RHFTextarea from '../textarea/RHFTextarea';
+import { useEffect } from 'react';
+import { FORM_FIELDS } from '../../constants/formFields';
 
 const BookReviewField = () => {
-  const { watch } = useFormContext();
-  const rating = Number(watch('rating'));
+  const { watch, trigger } = useFormContext();
+  const rating = Number(watch(FORM_FIELDS.BOOK_EVALUATION.RATING));
   const shouldWriteReview = rating <= 1 || rating >= 5;
+
+  useEffect(() => {
+    trigger(FORM_FIELDS.BOOK_EVALUATION.REVIEW);
+  }, [rating, trigger]);
 
   return (
     <div
@@ -18,8 +24,18 @@ const BookReviewField = () => {
     >
       <h2>독후감</h2>
       <RHFTextarea
-        name="review"
+        name={FORM_FIELDS.BOOK_EVALUATION.REVIEW}
         disabled={shouldWriteReview === false}
+        rules={{
+          required:
+            shouldWriteReview === true ? '독후감을 입력해주세요.' : false,
+          validate: (value) => {
+            if (shouldWriteReview === true && value.length < 100) {
+              return '독후감을 100자 이상 입력해주세요.';
+            }
+            return true;
+          },
+        }}
       />
     </div>
   );
