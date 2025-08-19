d14cd88d160e95528f9c4cef0e5c046f8d9d2679
lecto17 <khwoo_rpsit@naver.com>
2025-08-19 11:21:11 +0900

    refactor: RHFTextarea 컴포넌트 props 확장

--- client/src/components/textarea/RHFTextarea.tsx
+++ client/src/components/textarea/RHFTextarea.tsx
@@ -1,7 +1,12 @@
-import { Controller, FieldValues, useFormContext } from 'react-hook-form';
+import {
+  Controller,
+  ControllerRenderProps,
+  FieldError,
+  FieldValues,
+  useFormContext,
+} from 'react-hook-form';
 import ErrorMessage from '../common/ErrorMessage';
 import Textarea from './Textarea';
-import { useEffect } from 'react';
 import { FieldPath } from 'react-hook-form';
 
 type RHFTextareaProps<T extends FieldValues> = {
@@ -11,52 +16,54 @@ type RHFTextareaProps<T extends FieldValues> = {
     required?: string | boolean;
     validate?: (value: string) => string | boolean;
   };
+  render?: (
+    field: ControllerRenderProps<T, FieldPath<T>>,
+  ) => React.ReactElement;
 };
 
 export default function RHFTextarea<T extends FieldValues>({
   name,
   disabled,
   rules,
+  render,
   ...props
 }: RHFTextareaProps<T>) {
   const {
     control,
-    watch,
-    trigger,
     formState: { errors },
-  } = useFormContext<{ [key: string]: string }>();
+  } = useFormContext<T>();
 
-  const rating = Number(watch(`${name}`));
+  const getErrorMessage = () => {
+    const error = (errors as { [name: string]: FieldError })[name];
 
-  useEffect(() => {
-    trigger(name);
-  }, [rating, trigger]);
+    if (error?.message) {
+      return error.message;
+    }
+
+    return '';
+  };
 
   return (
     <>
       <Controller
         control={control}
         name={name}
-        rules={{
-          ...(rules || {
-            required: '독후감을 입력해주세요.',
-            validate: (value) => {
-              if (disabled == false && value.length < 100) {
-                return '독후감을 100자 이상 입력해주세요.';
-              }
-            },
-          }),
+        rules={rules}
+        render={({ field }) => {
+          if (render != undefined) {
+            return render(field);
+          }
+          return (
+            <>
+              <Textarea
+                {...field}
+                {...props}
+                disabled={disabled}
+              />
+              <ErrorMessage errorMessage={getErrorMessage()} />
+            </>
+          );
         }}
-        render={({ field }) => (
-          <>
-            <Textarea
-              {...field}
-              {...props}
-              disabled={disabled}
-            />
-            <ErrorMessage errorMessage={errors[name]?.message || ''} />
-          </>
-        )}
       />
     </>
   );
