2ce375db1ec98d79a8f26867e7a29f6770b09909
lecto17 <khwoo_rpsit@naver.com>
2025-08-19 14:15:21 +0900

    design: 이전, 다음 버튼 css 수정

--- client/src/components/book/StepperNavigation.tsx
+++ client/src/components/book/StepperNavigation.tsx
@@ -4,7 +4,6 @@ import { useRouter } from 'next/router';
 import { SubmitHandler, useFormContext } from 'react-hook-form';
 import { theme } from '@/theme';
 import { css } from '@emotion/react';
-import { getNextButtonDisabled } from '@/utils/validates';
 import { BookStatusFormData } from '../evaluation/BookStatusPeriodValidation';
 
 export default function StepperNavigation() {
@@ -27,10 +26,6 @@ export default function StepperNavigation() {
     handleSubmit,
   } = useFormContext<BookStatusFormData>();
 
-  // const watchedStatus = watch('status');
-  // const watchedStartDate = watch('startDate') || '';
-  // const watchedEndDate = watch('endDate') || '';
-
   const isPreviousButtonDisabled = (step as string) === minStep.toString();
 
   const onSubmit: SubmitHandler<BookStatusFormData> = (data) => {
@@ -41,7 +36,7 @@ export default function StepperNavigation() {
     <div css={bookStepperButtonWrapperStyle}>
       <button
         css={css`
-          background-color: ${theme.colors.background};
+          background-color: ${theme.colors.secondary};
           color: ${theme.colors.background};
           border: none;
           border-radius: ${theme.borderRadius.md};
@@ -49,10 +44,6 @@ export default function StepperNavigation() {
           cursor: pointer;
           font-size: ${theme.fontSize.sm};
           transition: all 0.3s ease;
-          &:disabled {
-            background-color: ${theme.colors.secondary};
-            cursor: not-allowed;
-          }
         `}
         onClick={handlePrevious}
         disabled={isPreviousButtonDisabled}
