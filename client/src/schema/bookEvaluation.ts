35da6dab1dd5baefafdf6b6241cf5dc0c748a981
lecto17 <khwoo_rpsit@naver.com>
2025-08-21 16:32:12 +0900

    feat: zod로 도서 시작일, 종료일 validation 작업

--- client/src/schema/bookEvaluation.ts
+++ client/src/schema/bookEvaluation.ts
@@ -1,16 +1,47 @@
-import { BOOK_STATUS_OPTIONS } from '@/constants/book';
+import { BOOK_STATUS_OPTIONS, PERIOD_REQUIREMENT } from '@/constants/book';
 import { z } from 'zod';
 
-export const bookStatusPeriodSchema = z.object({
-  status: z.enum(
-    BOOK_STATUS_OPTIONS.map((option) => option.value),
-    {
-      error: '독서 상태를 선택해주세요.',
-    },
-  ),
-  startDate: z.date().optional(),
-  endDate: z.date().optional(),
-});
+export const bookStatusPeriodSchema = z
+  .object({
+    status: z.enum(
+      BOOK_STATUS_OPTIONS.map((option) => option.value),
+      {
+        error: '독서 상태를 선택해주세요.',
+      },
+    ),
+    startDate: z.string().optional(),
+    endDate: z.string().optional(),
+  })
+  .superRefine((data, ctx) => {
+    const [isStartDateRequired, isEndDateRequired] =
+      PERIOD_REQUIREMENT[data.status];
+
+    if (isStartDateRequired && !data.startDate) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '독서 시작일을 선택해주세요.',
+        path: ['startDate'],
+      });
+    }
+
+    if (isEndDateRequired && !data.endDate) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '독서 종료일을 선택해주세요.',
+        path: ['endDate'],
+      });
+    }
+
+    if (data.startDate && data.endDate) {
+      if (data.startDate > data.endDate) {
+        ctx.addIssue({
+          code: 'custom',
+          message: '독서 시작일이 종료일보다 늦습니다.',
+          path: ['startDate'],
+        });
+      }
+    }
+  });
 
 export const bookRatingReviewSchema = z
   .object({
