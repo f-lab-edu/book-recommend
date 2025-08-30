8cc307de135ea1568d1e486018f40e40a6c2e04f
lecto17 <khwoo_rpsit@naver.com>
2025-08-30 16:21:00 +0900

    chore: 홈 화면 내 컴포넌트 별 AsyncBoundary 래핑

--- client/src/components/home/HomeMain.tsx
+++ client/src/components/home/HomeMain.tsx
@@ -2,8 +2,9 @@ import { css } from '@emotion/react';
 import BestSellerCarousel from '../carousel/BestSellerCarousel';
 import NewBookList from '../list/NewBookList';
 import RecommenMusicList from '../list/RecommenMusicList';
+import AsyncBoundary from '../boundary/AsyncBoundary';
 
-const HomeMain = () => {
+const HomeMain = ({ error }: { error: { message: string } }) => {
   return (
     <div
       css={css`
@@ -24,9 +25,20 @@ const HomeMain = () => {
           background-color: #f0f0f0;
         `}
       >
-        <BestSellerCarousel />
-        <NewBookList />
-        <RecommenMusicList />
+        {/* 
+          Step 1. 베스트셀러 API 연동
+          Step 2. 신간 도서 리스트 API 연동
+          Step 3. 추천 음악 리스트 API 연동(viewport 감지 시)
+        */}
+        <AsyncBoundary>
+          <BestSellerCarousel />
+        </AsyncBoundary>
+        <AsyncBoundary>
+          <NewBookList />
+        </AsyncBoundary>
+        <AsyncBoundary>
+          <RecommenMusicList />
+        </AsyncBoundary>
       </div>
     </div>
   );
