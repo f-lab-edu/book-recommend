083ee9ac3723646606d02bd9c6a6382ba6157585
lecto17 <khwoo_rpsit@naver.com>
2025-08-30 16:17:57 +0900

    chore: BookCard 컴포넌트 내 aladin api response 적용

--- client/src/components/book/BookCard.tsx
+++ client/src/components/book/BookCard.tsx
@@ -1,37 +1,48 @@
-import Image from "next/image"
-import { Book } from "@/types/book"
-import { bookCardStyle, bookImageContainerStyle } from "@/styles/bookList.styles"
-import Link from "next/link"
-import { concatIsbn } from "@/utils/utils"
-import React from "react"
+import Image from 'next/image';
+import { AladinBook } from '@/types/book';
+import {
+  bookCardStyle,
+  bookImageContainerStyle,
+} from '@/styles/bookList.styles';
+import Link from 'next/link';
+import { concatIsbn } from '@/utils/utils';
+import React from 'react';
 
 type BookCardProps = {
-  book: Book
-}
+  book: AladinBook;
+};
 
-const BookCard = React.memo(({ book }: BookCardProps) => {
-  return (
-    <Link href={`/books/${concatIsbn(book.isbn)}?step=1`}>
-      <div css={bookCardStyle}>
-        <div css={bookImageContainerStyle}>
-          <Image src={book.thumbnail} alt={book.title} width={100} height={100} />
+const BookCard = React.memo(
+  ({ book }: BookCardProps) => {
+    return (
+      <Link href={`/books/${concatIsbn(book.isbn)}?step=1`}>
+        <div css={bookCardStyle}>
+          <div css={bookImageContainerStyle}>
+            <Image
+              src={book.cover}
+              alt={book.title}
+              width={100}
+              height={100}
+            />
+          </div>
+          <h3>{book.title}</h3>
+          <p>{book.author}</p>
+          <p>{book.publisher}</p>
+          <p>{book.priceSales}</p>
         </div>
-        <h3>{book.title}</h3>
-        <p>{book.authors.join(", ")}</p>
-        <p>{book.publisher}</p>
-        <p>{book.price}</p>
-      </div>
-    </Link>
-  )
-}, (prevProps, nextProps) => {
-  return (
-    prevProps.book.title === nextProps.book.title &&
-    prevProps.book.sale_price === nextProps.book.sale_price &&
-    prevProps.book.thumbnail === nextProps.book.thumbnail &&
-    prevProps.book.status === nextProps.book.status
-  );
-})
+      </Link>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.book.title === nextProps.book.title &&
+      prevProps.book.priceSales === nextProps.book.priceSales &&
+      prevProps.book.cover === nextProps.book.cover &&
+      prevProps.book.stockStatus === nextProps.book.stockStatus
+    );
+  },
+);
 
-BookCard.displayName = 'BookCard'
+BookCard.displayName = 'BookCard';
 
-export default BookCard
+export default BookCard;
