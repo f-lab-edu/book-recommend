31add0f0a210bc394147e1903c0374be67f0fd5c
lecto17 <khwoo_rpsit@naver.com>
2025-08-30 10:59:54 +0900

    chore: 홈화면 내 best-seller 정보 prefetch

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,22 +1,73 @@
-import BookList from "@/components/book/BookList";
-import { BookListErrorFallback } from "@/components/common/ErrorFallbacks";
-import SuspenseBoundary from "@/components/common/SuspenseBoundary";
-import Head from "next/head";
+import BookList from '@/components/book/BookList';
+import { BookListErrorFallback } from '@/components/common/ErrorFallbacks';
+import SuspenseBoundary from '@/components/common/SuspenseBoundary';
+import HomeMain from '@/components/home/HomeMain';
 
-export default function Home() {
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import {
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { dehydrate } from '@tanstack/react-query';
+import { getBestSellerBooks } from '@/remotes/book';
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  query,
+}) => {
+  try {
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery({
+      queryKey: ['bestseller'],
+      queryFn: () => getBestSellerBooks(),
+    });
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  } catch (error) {
+    console.error('getServerSideProps error:', error);
+    return {
+      props: {
+        dehydratedState: {},
+      },
+    };
+  }
+};
+
+export default function Home({
+  dehydratedState,
+}: {
+  dehydratedState: DehydratedState;
+}) {
   return (
     <>
       <Head>
         <title>Book Recommend</title>
-        <meta name="description" content="Book Recommend to Frontend Developer" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Book Recommend to Frontend Developer"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
       </Head>
 
-      <SuspenseBoundary
+      <HydrationBoundary state={dehydratedState}>
+        <HomeMain />
+      </HydrationBoundary>
+
+      {/* <SuspenseBoundary
         rejectedFallback={BookListErrorFallback}
       >
         <BookList />
-      </SuspenseBoundary>
+      </SuspenseBoundary> */}
     </>
   );
 }
