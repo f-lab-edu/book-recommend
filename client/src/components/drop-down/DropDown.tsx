1f60ab49786309a450863cb28b4bffbfee170659
lecto17 <khwoo_rpsit@naver.com>
2025-09-04 12:32:45 +0900

    chore: Dropdown 컴포넌트 공용화 작업 중

--- client/src/components/drop-down/DropDown.tsx
+++ client/src/components/drop-down/DropDown.tsx
@@ -1,8 +1,7 @@
-import useDropdown from '@/hooks/useDropdownState';
 import { theme } from '@/theme';
 import { css } from '@emotion/react';
 import DropdownList from './DropdownList';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import useClickOutside from '@/hooks/useClickOutside';
 
 type DropdownProps = {
@@ -10,49 +9,71 @@ type DropdownProps = {
   defaultValue?: string;
   onChange: (value: string) => void;
   isError?: boolean;
+  needsFolded?: boolean;
+  isFetching?: boolean;
 };
+
+export type HandleItemSelectType = ({
+  label,
+  value,
+}: {
+  label: string;
+  value: string;
+}) => void;
 
 export default function Dropdown({
   options,
   defaultValue,
   onChange,
   isError,
+  needsFolded = false,
+  isFetching = false,
 }: DropdownProps) {
   const dropDownRef = useRef<HTMLDivElement>(null);
-  const {
-    isOpen,
-    activeValue,
-    handleDropdownOpen,
-    handleDropdownClose,
-    handleItemSelect,
-  } = useDropdown({
-    defaultValue,
-    onChange,
-  });
+  const [activeValue, setActiveValue] = useState<string>(defaultValue || '');
+  const [isOpen, setIsOpen] = useState(needsFolded ? false : true);
+
+  const handleDropdownOpen = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setIsOpen(true);
+  };
+
+  const handleDropdownClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleItemSelect: HandleItemSelectType = ({ label, value }) => {
+    setActiveValue(label);
+    handleDropdownClose();
+    onChange(value);
+  };
+
   useClickOutside({ ref: dropDownRef, callback: handleDropdownClose });
 
   return (
     <div
       css={css`
         position: relative;
-        height: 40px;
         width: 100%;
-        border: 1px solid ${isError ? theme.colors.error : theme.colors.primary};
+        // height: 40px;
+        // border: 1px solid ${isError
+          ? theme.colors.error
+          : theme.colors.primary};
         border-radius: ${theme.borderRadius.sm};
         padding: ${theme.spacing.sm} ${theme.spacing.md};
-        cursor: pointer;
       `}
       ref={dropDownRef}
       onClick={handleDropdownOpen}
     >
-      {isOpen ? (
-        <DropdownList
-          options={options}
-          handleClick={handleItemSelect}
-        />
-      ) : (
-        <span>{activeValue || defaultValue || '선택'}</span>
-      )}
+      {/* {isOpen ? ( */}
+      <DropdownList
+        options={options}
+        handleClick={handleItemSelect}
+        isFetching={isFetching}
+      />
+      {/* ) : (
+        <span>{activeValue || defaultValue || '선택1'}</span>
+      )} */}
     </div>
   );
 }
--- client/src/components/drop-down/DropDownList.tsx
+++ client/src/components/drop-down/DropDownList.tsx
@@ -1,17 +1,20 @@
-import { HandleItemSelectType } from '@/hooks/useDropdownState';
+import { HandleItemSelectType } from './Dropdown';
 
 import DropdownItem from './DropdownItem';
 import { theme } from '@/theme';
 import { css } from '@emotion/react';
+import Loading from '../common/Loading';
 
 type DropdownListProps = {
   options: readonly { label: string; value: string }[];
   handleClick: HandleItemSelectType;
+  isFetching?: boolean;
 };
 
 export default function DropdownList({
   options,
   handleClick,
+  isFetching,
 }: DropdownListProps) {
   return (
     <div
@@ -19,15 +22,49 @@ export default function DropdownList({
         position: absolute;
         top: 100%;
         left: 0;
-        right: 0;
-        background: white;
-        border-top: none;
-        border-radius: 0 0 ${theme.borderRadius.sm} ${theme.borderRadius.sm};
+        // border: 1px solid ${theme.colors.secondary};
+        // border-top: none;
+        // padding: ${theme.spacing.sm} ${theme.spacing.md};
+        // border-radius: 0 0 ${theme.borderRadius.sm} ${theme.borderRadius.sm};
+        width: 100%;
+        height: ${isFetching ? '200px' : options.length > 0 ? 'auto' : '0px'};
         max-height: 200px;
         overflow-y: auto;
-        z-index: 1000;
+        background: white;
+        z-index: 10;
+
+        ${isFetching
+          ? css`
+              height: 200px;
+              border: 1px solid ${theme.colors.secondary};
+            `
+          : `${
+              options.length == 0
+                ? css`
+                    height: 0px;
+                    border: none;
+                  `
+                : css`
+                    height: auto;
+                    border: 1px solid ${theme.colors.secondary};
+                  `
+            }
+        `}
       `}
     >
+      {isFetching && (
+        <div
+          css={css`
+            display: flex;
+            width: 100%;
+            height: 100%;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          <Loading />
+        </div>
+      )}
       {options.map((option) => (
         <DropdownItem
           key={option.value}
