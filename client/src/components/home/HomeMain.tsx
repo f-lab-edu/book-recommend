8bd8a2f84be83762bd3f1da4afa611fa80ade151
lecto17 <khwoo_rpsit@naver.com>
2025-09-04 12:33:19 +0900

    chore: 컴포넌트 명 오탈자 수정

--- client/src/components/home/HomeMain.tsx
+++ client/src/components/home/HomeMain.tsx
@@ -1,26 +1,25 @@
 import { css } from '@emotion/react';
 import BestSellerCarousel from '../carousel/BestSellerCarousel';
 import NewBookList from '../list/NewBookList';
-import RecommenMusicList from '../list/RecommenMusicList';
+import RecommendMusicList from '../list/RecommendMusicList';
 import AsyncBoundary from '../boundary/AsyncBoundary';
 
 const HomeMain = ({ error }: { error: { message: string } }) => {
   return (
     <>
-      {/* 
-          Step 1. 베스트셀러 API 연동
-          Step 2. 신간 도서 리스트 API 연동
-          Step 3. 추천 음악 리스트 API 연동(viewport 감지 시)
-        */}
       <AsyncBoundary>
-        <BestSellerCarousel />
+        <BestSellerCarousel error={error} />
       </AsyncBoundary>
       <AsyncBoundary>
         <NewBookList />
       </AsyncBoundary>
-      <AsyncBoundary>
-        <RecommenMusicList />
-      </AsyncBoundary>
+      {/* 
+          TODO
+          Step 3. 추천 음악 리스트 API 연동(viewport 감지 시)
+        */}
+      {/* <AsyncBoundary>
+        <RecommendMusicList />
+      </AsyncBoundary> */}
     </>
   );
 };
