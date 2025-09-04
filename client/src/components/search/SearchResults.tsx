dbff06d58fc6dc956830f7e0778e61c23adfd07b
lecto17 <khwoo_rpsit@naver.com>
2025-09-04 12:35:03 +0900

    chore: 도서 검색 결과 컴포넌트 생성

---
+++ client/src/components/search/SearchResults.tsx
@@ -0,0 +1,74 @@
+import { css } from '@emotion/react';
+import { AladinBook, Book } from '@/types/book';
+import BookList from '../book/BookList';
+import Loading from '../common/Loading';
+import ErrorMessage from '../common/ErrorMessage';
+import { theme } from '@/theme';
+import BookCard from '../book/BookCard';
+import Grid from '../layout/Grid';
+
+interface SearchResultsProps {
+  results: AladinBook[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const SearchResults = ({ results, isLoading, error }: SearchResultsProps) => {
+  console.log('BBOOKK results: ', results);
+  if (isLoading) {
+    return (
+      <div
+        css={css`
+          margin-top: ${theme.spacing.lg};
+        `}
+      >
+        <Loading />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div
+        css={css`
+          margin-top: ${theme.spacing.lg};
+        `}
+      >
+        <ErrorMessage message={error} />
+      </div>
+    );
+  }
+
+  if (results.length === 0) {
+    return (
+      <div
+        css={css`
+          margin-top: ${theme.spacing.lg};
+          text-align: center;
+          color: ${theme.colors.text.secondary};
+        `}
+      >
+        검색 결과가 없습니다.
+      </div>
+    );
+  }
+
+  return (
+    <div
+      css={css`
+        margin-top: ${theme.spacing.lg};
+      `}
+    >
+      <Grid cols={2}>
+        {results.map((result) => (
+          <BookCard
+            key={result.isbn}
+            book={result}
+          />
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default SearchResults;
